bc6527eea585b56958ffef790b2b33469b0daece
bmv50 <86429212+bmv50@users.noreply.github.com>
2021-06-28 09:39:09 +0300

    Initial commit

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,13 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+    public static void compareNumbers() {
+        int a = 25, b = 20;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
