4b9431ee2c4aa133f65516ea3b3527f5e4e98c36
bmv50 <86429212+bmv50@users.noreply.github.com>
2021-07-07 20:19:53 +0300

    Четвертая домашка, поле 3х3 работает, но почему-то на большем количестве полей на работаю условия победы и поражения (ничья срабатывает).

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,110 +1,117 @@
-import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class HomeWorkApp {
+    static final int SIZE = 3;
+    static final char PLAYER = 'O';
+    static final char COMP = 'X';
+    static final char NULL = '.';
+    static Scanner vvod = new Scanner(System.in);
+    static Random random = new Random();
+    static final char[][] POLE = new char[SIZE][SIZE];
+
     public static void main(String[] args) {
-        invertArray();
-        fillArray();
-        changeArray();
-        fillDiagonal();
-        initialValueArrow(6, 7);
-        minMaxArrow();
-        System.out.println(checkBalance(new int[5]));
-    }
-
-    // 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
-    // Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
-    public static void invertArray() {
-        System.out.println("\nЗадание 1");
-        int[] arr = {1, 0, 1, 0, 0, 1, 0, 0};
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                arr[i] = 1;
-            } else {
-                arr[i] = 0;
+        initfield();
+        printField();
+        while (true) {
+            movePerson();
+            printField();
+            if (checkWin(PLAYER)) {
+                System.out.println("Вы победили");
+                break;
+            }
+            if (isFreeSpace()) {
+                System.out.println("Ничья");
+                break;
+            }
+            moveAi();
+            System.out.println();
+            printField();
+            if (checkWin(COMP)) {
+                System.out.println("Вы проиграли");
+                break;
+            }
+            if (isFreeSpace()) {
+                System.out.println("Ничья");
+                break;
             }
         }
-        System.out.println(Arrays.toString(arr));
     }
 
-    // 2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
-    public static void fillArray() {
-        System.out.println("\nЗадание 2");
-        int[] arr = new int[100];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
-        }
-        System.out.println(Arrays.toString(arr));
-    }
-
-    //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
-    public static void changeArray() {
-        System.out.println("\nЗадание 3");
-        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 6) {
-                arr[i] = arr[i] * 2;
+    public static void initfield() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                POLE[i][j] = NULL;
             }
         }
-        System.out.println(Arrays.toString(arr));
     }
 
-    //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
-    // и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей,
-    // если обе сложно). Определить элементы одной из диагоналей можно по следующему принципу:
-    // индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
-    public static void fillDiagonal() {
-        System.out.println("\nЗадание 4");
-        int[][] arr = new int[4][4];
-        for (int i = 0; i < 4; i++) {
-            arr[i][i] = 1;
-        }
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++)
-                System.out.printf("%3d", arr[i][j]);
+    public static void printField() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(POLE[i][j] + "\t");
+            }
             System.out.println();
         }
     }
 
-    //5. Написать метод, принимающий на вход два аргумента: len и initialValue,
-    // и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
-    public static void initialValueArrow(int len, int initialValue) {
-        System.out.println("\nЗадание 5");
-        int[] arr = new int[len];
-        for (int i = 0; i < arr.length; i++){
-            arr[i] = initialValue;
-        }
-        System.out.println(Arrays.toString(arr));
-        }
+    public static void setSym(int x, int y, char sym) {
+        POLE[y][x] = sym;
+    }
 
-        //6. * Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
-        public static void minMaxArrow() {
-            System.out.println("\nЗадание 6");
-            int[] arr = {7, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 5};
-            int max = arr[0];
-            int min = arr[0];
-            for (int i = 0; i != arr.length; i++){
-                if(arr[i] > max){
-                    max = arr[i];
-                }
-                if(arr[i] < min){
-                    min = arr[i];
-                }
+    public static void movePerson() {
+        int x, y;
+        do {
+            x = vvod.nextInt() - 1;
+            y = vvod.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        POLE[y][x] = PLAYER;
+    }
+
+    public static void moveAi() {
+        int x, y;
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        POLE[y][x] = COMP;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (POLE[y][x] == NULL) return true;
+        return false;
+    }
+
+    public static boolean isFreeSpace() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (POLE[i][j] == NULL) return false;
             }
-            System.out.println("Мининальный элемент: " + min);
-            System.out.println("Максимальный элемент: " + max);
         }
-    //7. ** Написать метод, в который передается не пустой одномерный целочисленный массив,
-    // метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
-    public static boolean checkBalance(int[] arr) {
-        System.out.println("\nЗадание 7");
-        int leftSum = 0;
-        int rightSum = 0;
-        for (int i = 0, j = 0; i < arr.length - j; i++)
-        {
-            leftSum += arr[i];
-            if (leftSum > rightSum)
-                rightSum += arr[arr.length - 1 - j++];
+        return true;
+    }
+
+    public static boolean checkWin(char sym) {
+        for (int i = 0; i < SIZE; i++) {
+            int result = 0;
+            for (int j = 0; j < SIZE; j++) {
+                if (POLE[i][j] == sym) result++;
+            }
+            if (result == SIZE) return true;
         }
-        return rightSum==leftSum;
+        int first = 0;
+        for (int i = 0; i < SIZE; i++) {
+            int result = 0;
+            for (int j = 0; j < SIZE; j++) {
+                if (j == i && POLE[i][j] == sym) first++;
+            }
+        }
+        if (first == SIZE) return true;
+        int second = 0;
+        for (int i = 0, j = SIZE - 1; i < SIZE && j >= 0; i++, j--) {
+        }
+        if (second == SIZE) return true;
+        return false;
     }
 }
