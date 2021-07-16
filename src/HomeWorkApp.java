c40d9846fb169adcda17b239f56a3ebc6c88cb64
bmv50 <86429212+bmv50@users.noreply.github.com>
2021-07-16 19:17:42 +0300

    Шестая списанная домашка

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,46 +1,20 @@
+public class HomeWorkApp
+{
+    public static void main(String[] args)
+    {
+        AnimalFactory factory = new AnimalFactory();
 
-public class HomeWorkApp {
+        Cat cat = factory.newCat(200);
+        Dog dog = factory.newDog(500, 10);
 
-    public static void main(String[] args) {
-        Employee[] employees = {
-                new Employee(
-                        "Иванов Иван Иванович",
-                        "Ген.директор",
-                        "gendirector@gmail.com",
-                        "+79253567894",
-                        1000000,
-                        45),
-                new Employee("Сидоров Сидор Сидорович",
-                        "Зам. ген.директора",
-                        "zamgendirector@gmail.com",
-                        "+79602569841",
-                        900000,
-                        40),
-                new Employee("Александров Александр Александрович",
-                        "Финансовый директор",
-                        "findirector@gmail.com",
-                        "+79203567844",
-                        800000,
-                        42),
-                new Employee("Иванова Светлана Ивановна",
-                        "Главный бухгалтер",
-                        "glavbuh@gmail.com",
-                        "+79253697812",
-                        500000,
-                        35),
-                new Employee("Романов Роман Романович",
-                        "Водитель",
-                        "driver@gmail.com",
-                        "+79201254120",
-                        10000,
-                        21)
-        };
-        int minAge = 40;
-        for (Employee employee : employees) {
-            if (employee.age > minAge) {
-                employee.printText();
-                System.out.println();
-            }
-        }
+        cat.runOn(150);
+        cat.swimOn(5);
+
+        dog.runOn(550);
+        dog.swimOn(9);
+
+        System.out.println("Животных создано: " + factory.getAnimalsCount());
+        System.out.println("Собак создано: " + factory.getDogsCount());
+        System.out.println("Кошек создано: " + factory.getCatsCount());
     }
 }
